8451b38a1d4d55018fd48362d7c3a65090a1b60c
mckinlec <112263595+mckinlec@users.noreply.github.com>
2023-06-03 06:15:13 -0400

    Update docker-helper.sh

--- docker-standalone/docker-helper.sh
+++ docker-standalone/docker-helper.sh
@@ -44,4 +44,4 @@ if [[ -v KOBOLDAI_MODELDIR ]];then
 	#ln -s $KOBOLDAI_MODELDIR/cache/ cache
 fi
 
-PYTHONUNBUFFERED=1 ./play.sh --remote --quiet --override_delete --override_rename
+PYTHONUNBUFFERED=1 ./play.sh --host  --verbosity --override_delete --override_rename
